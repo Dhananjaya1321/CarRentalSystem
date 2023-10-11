a3d7dea9e27ba48f3e33185058ba59d741f411f5
Dhananjaya1321 <blackisurudananjaya@gmail.com>
2023-10-11 21:55:25 +0530

    first commit

---
+++ Back_End/src/main/java/lk/ijse/car_rental_system/WebAppInitializer.java
@@ -0,0 +1,19 @@
+package lk.ijse.car_rental_system;
+
+import lk.ijse.car_rental_system.config.WebAppConfig;
+import lk.ijse.car_rental_system.config.WebRootConfig;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{WebRootConfig.class};
+    }
+
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{WebAppConfig.class};
+    }
+
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
