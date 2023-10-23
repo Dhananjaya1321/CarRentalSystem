73b109b3f98c336b73db4aaae65d76312885878e
Dhananjaya1321 <blackisurudananjaya@gmail.com>
2023-10-23 16:22:03 +0530

    Added CommonsMultipartResolver for context and test image uploading

--- Back_End/src/main/java/lk/ijse/car_rental_system/config/WebAppConfig.java
+++ Back_End/src/main/java/lk/ijse/car_rental_system/config/WebAppConfig.java
@@ -1,12 +1,21 @@
 package lk.ijse.car_rental_system.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = "lk.ijse.car_rental_system.controller")
 public class WebAppConfig {
-
+    @Bean
+    public CommonsMultipartResolver multipartResolver() {
+        CommonsMultipartResolver resolver = new CommonsMultipartResolver();
+        resolver.setDefaultEncoding("utf-8");
+        resolver.setMaxUploadSize(10485760);
+        resolver.setMaxInMemorySize(1048576);
+        return resolver;
+    }
 }
--- Back_End/src/main/java/lk/ijse/car_rental_system/controller/UserController.java
+++ Back_End/src/main/java/lk/ijse/car_rental_system/controller/UserController.java
@@ -3,10 +3,11 @@ package lk.ijse.car_rental_system.controller;
 import lk.ijse.car_rental_system.service.UserService;
 import lk.ijse.car_rental_system.util.ResponseUtil;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/user")
@@ -16,7 +17,24 @@ public class UserController {
     UserService user;
 
     @GetMapping(params = {"username"})
-    public ResponseUtil findUser(String username){
-        return new ResponseUtil("Ok","",user.findUser(username));
+    public ResponseUtil findUser(String username) {
+        return new ResponseUtil("Ok", "", user.findUser(username));
     }
+
+   /* @PostMapping("/upload")
+    public ResponseUtil uploadFile(@RequestParam("file") MultipartFile file) {
+        if (file.isEmpty()) {
+            return new ResponseUtil("", "", "");
+        }
+
+        String uploadDir = "C:\\Users\\ACER\\Documents\\WorkZone\\CarRentalSystem\\Back_End\\src\\main\\resources\\files\\" + "upload-dir";
+        try {
+            String fileName = file.getOriginalFilename();
+            String filePath = new File(uploadDir, fileName).getAbsolutePath();
+            file.transferTo(new File(filePath));
+            return new ResponseUtil("", "", "");
+        } catch (IOException e) {
+            return new ResponseUtil("", "", "");
+        }
+    }*/
 }
