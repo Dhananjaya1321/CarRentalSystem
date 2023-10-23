f030674fa3d855ecfbedd6e88e73478ed127477c
Dhananjaya1321 <blackisurudananjaya@gmail.com>
2023-10-23 23:16:47 +0530

    Created loadAllDriversForTable function

--- Front_End/controller/driverController.js
+++ Front_End/controller/driverController.js
@@ -7,12 +7,33 @@ function getAllDrivers() {
         method: "get",
         success: function (rep) {
             drivers = rep.data;
-            console.log(drivers)
+            loadAllDriversForTable(drivers);
         },
         error: function (rep) {
 
         }
     });
+}
+
+function loadAllDriversForTable(array) {
+    $("#driver-details-table-body").empty();
+    for (const i in array) {
+        let driver = array[i];
+        let row = `<tr>
+                        <td>${driver.driver_id}</td>
+                        <td>${driver.nic}</td>
+                        <td>${driver.name}</td>
+                        <td>${driver.contact}</td>
+                        <td>${driver.address}</td>
+                        <td>${driver.email}</td>
+                        <td>
+                            <button type="button" class="btn btn-danger border-0" style="background-color: #ff0014">
+                            <i class="fa-solid fa-trash-can"></i>
+                            </button>
+                        </td>
+                    </tr>`;
+        $("#driver-details-table-body").append(row);
+    }
 }
 
 function generateDriverID() {
@@ -74,7 +95,7 @@ $("#driver-add-btn").click(function () {
     let password = $("#driver-password").val();
     let email = $("#driver-email").val();
     let driver_id = generateDriverID();
-    if (!searchUsername(username)){
+    if (!searchUsername(username)) {
         let newDriver = {
             "driver_id": driver_id,
             "address": address,
@@ -93,12 +114,13 @@ $("#driver-add-btn").click(function () {
             success: function (rep) {
                 alert(rep.message);
                 getAllDrivers();
+                loadAllDriversForTable(drivers);
             },
             error: function (rep) {
                 alert(rep.responseJSON.message)
             }
         });
-    }else {
+    } else {
         /*username already excites*/
     }
 });
